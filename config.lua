0271a15e46cc1d70cf9e1028e685cc4098d464e7
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-12-01 11:33:36 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Speed unit (false = KM/H, true = MPH)
+Config.UseMPH = true
+
+-- Framework auto-detection ('qbcore', 'qbox', or 'auto')
+Config.Framework = 'auto'
+
+-- Show logo at top center
+Config.ShowLogo = true
