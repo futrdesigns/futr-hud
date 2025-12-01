e142f5bdb35ccb66a104519df1fad2751e9ebefd
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-12-01 11:32:52 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'futrdesigns'
+description 'Simple HUD with Player Info'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+files {
+    'html/index.html',
+    'html/assets/css/*.css',
+    'html/assets/js/*.js',
+    'html/assets/img/*'
+}
+
+ui_page 'html/index.html'
