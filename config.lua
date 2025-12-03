8676a7b011962f3bc9e04486037d37b5693d9655
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-12-03 18:40:44 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -9,3 +9,9 @@ Config.Framework = 'auto'
 
 -- Show logo at top center
 Config.ShowLogo = false
+
+-- Clock Settings
+Config.ShowClock = true -- Show/hide the clock
+Config.ClockFormat = '12' -- '12' for 12-hour format or '24' for 24-hour format
+Config.TimeZone = 'GMT' -- Time zone: 'GMT', 'UTC', 'EST', 'PST', 'CST', 'MST', etc.
+Config.ShowSeconds = false -- Show seconds in the clock
