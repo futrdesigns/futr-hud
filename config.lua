4caf7f7af6b3cb932456343520992092511680ed
futrdesigns <99038220+futrdesigns@users.noreply.github.com>
2025-12-04 18:25:43 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {}
 
 -- Speed unit (false = KM/H, true = MPH)
-Config.UseMPH = false
+Config.UseMPH = true
 
 -- Framework auto-detection ('qbcore', 'qbox', 'esx', or 'auto')
 -- 'auto' will automatically detect which framework is running
@@ -12,6 +12,6 @@ Config.ShowLogo = false
 
 -- Clock Settings
 Config.ShowClock = true -- Show/hide the clock
-Config.ClockFormat = '12' -- '12' for 12-hour format or '24' for 24-hour format
+Config.ClockFormat = '24' -- '12' for 12-hour format or '24' for 24-hour format
 Config.TimeZone = 'GMT' -- Time zone: 'GMT', 'UTC', 'EST', 'PST', 'CST', 'MST', etc.
 Config.ShowSeconds = false -- Show seconds in the clock
